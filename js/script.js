5f6be4e0ca0723b060b2298f168b1732adf1145e
webcoding_by <webcoding.by@gmail.com>
2020-07-24 22:04:11 +0300

    validation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 //                                                        -----__________Task #1____________------
 let userName = prompt('Enter your Login');
 
-console.log(userName);
-while(userName.lenght > 10 && userName.length !== '') {
-  alert('Длинна Логина недолжна привышать 10 символов');
+
+while (userName.length > 10 && userName.length < 3) {
+  alert('Длинна Логина недолжна привышать 10 символов и бменьше 3 символов');
   userName = prompt('Enter your Login');
 }
-let userPassword = prompt('Enter your Password');
+while (userName == '' || userName == null) {
+  alert('Логин не введен');
+  userName = prompt('Enter your Login');
+}
+
+let userPassword = +prompt('Enter your Password');
+
+while(userPassword == '' || userPassword == null) {
+  alert('Пароль не введен');
+  userPassword = +prompt('Enter your Password');
+}
 
 let user = {
   login: userName,
@@ -19,10 +29,10 @@ while (userAuthName !== user.login) {
   userAuthName = prompt('Enter your Login again');
 }
 
-userAuthPassword = prompt('Enter your Password Auth');
+userAuthPassword = +prompt('Enter your Password Auth');
 while (userAuthPassword !== user.password) {
   alert('Не правильный пароль, попробуйте еще раз');
-  userAuthPassword = prompt('Enter your Password again');
+  userAuthPassword = +prompt('Enter your Password again');
 }
 
 console.log(`Добро пожаловать ${userName}`);
@@ -33,14 +43,52 @@ console.log(`Добро пожаловать ${userName}`);
 //                                                        -----__________Task #2____________------
 
 
-var persons = [
-  {id: 10, name: 'Vasya', age: 28},
-  {id: 11, name: 'Kolya', age: 16, parentId: 14},
-  {id: 12, name: 'Anna', age: 37},
-  {id: 13, name: 'Zoya', age: 9, parentId: 12},
-  {id: 14, name: 'Arkadiy', age: 45},
-];
+// var persons = [{
+//     id: 10,
+//     name: 'Vasya',
+//     age: 28
+//   },
+//   {
+//     id: 11,
+//     name: 'Kolya',
+//     age: 16,
+//     parentId: 14
+//   },
+//   {
+//     id: 12,
+//     name: 'Anna',
+//     age: 37
+//   },
+//   {
+//     id: 13,
+//     name: 'Zoya',
+//     age: 9,
+//     parentId: 12
+//   },
+//   {
+//     id: 14,
+//     name: 'Arkadiy',
+//     age: 45
+//   },
+// ];
 
-for(let i = 0; i < persons.lenght; i++){
-  console.log(persons[i]);
-}
+
+
+
+// for (let i = 0; i < persons.length; i++) {
+//   let childName = persons[i].name;
+//   let childId = persons[i].parentId;
+//   let parentId = persons[i].id;
+
+//   console.log(parentId);
+//   console.log(childId);
+
+//   if (persons[i].age >= 18) {
+//     userProf = prompt(`${persons[i].name} кем вы работает?`)
+//     console.log(persons[i].id);
+//     persons[i].childName = childName;
+//   } else {
+
+//   }
+//   console.log(persons[i]);
+// }
